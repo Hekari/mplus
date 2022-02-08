bdba1a84f4757bcbc08dd16a5757b8eea9021799
Hekari <sszpytma6@gmail.com>
2022-02-08 23:03:23 +0100

    about section work in progress

--- main.js
+++ main.js
@@ -1,25 +1,23 @@
-const navbar=document.querySelector('nav')
-const hero=document.querySelector('#HeroBig')
-const navlink=document.querySelectorAll('.nav-link')
-const navbrand=document.querySelector('.nav-brand')
-const addBg = () => {
-    if(window.scrollY>100){
-        navbar.classList.add('addBg')
-        navlink.forEach(nav=>{nav.style.color="white"
-        nav.addEventListener('mouseenter',()=>{nav.style.color='#ffba08'})
-        nav.addEventListener('mouseleave',()=>{nav.style.color='white'})
-        if(navbar.classList.contains(addBg)){
-            navbrand.style.color='white'
-        }
-    })
-    }else if(window.scrollY<100){
-        navbar.classList.remove('addBg')
-        navbar.style.transition='.3s'
-        navlink.forEach(nav=>{nav.style.color="black"
-        nav.addEventListener('mouseenter',()=>{nav.style.color="#ffba0"})
-        nav.addEventListener('mouseleave',()=>{nav.style.color='black'})
-    })
-    }
-}
-// document.addEventListener('DOMContentLoaded',addBg)
-window.addEventListener('scroll',addBg)
+// const navbar=document.querySelector('nav')
+// const hero=document.querySelector('#HeroBig')
+// const navlink=document.querySelectorAll('.nav-link')
+// const navbrand=document.querySelector('.navbar-brand')
+// const bars=document.querySelector('.fa-bars')
+// const addBg = () => {
+//     if(window.scrollY>=0){
+//         navbar.classList.add('addBg')
+//         navlink.forEach(nav=>{nav.style.color="black"
+//         nav.addEventListener('mouseenter',()=>{nav.style.color='#ffda79'})
+//         nav.addEventListener('mouseleave',()=>{nav.style.color='black'})
+//     })
+//     }else if(window.scrollY<100){
+//         navbar.classList.remove('addBg')
+//         navbar.style.transition='.3s'
+//         navlink.forEach(nav=>{nav.style.color="black"
+//         nav.addEventListener('mouseenter',()=>{nav.style.color="#ffda79"})
+//         nav.addEventListener('mouseleave',()=>{nav.style.color='black'})
+//     })
+//     }
+// }
+// // document.addEventListener('DOMContentLoaded',addBg)
+// window.addEventListener('DOMContentLoaded',addBg)
