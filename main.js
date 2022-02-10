beac78dcb893a76331db1a38d9c22466049aa1df
Hekari <sszpytma6@gmail.com>
2022-02-10 19:59:57 +0100

    cool button added FINALLY

--- main.js
+++ main.js
@@ -1,23 +1,6 @@
-// const navbar=document.querySelector('nav')
-// const hero=document.querySelector('#HeroBig')
-// const navlink=document.querySelectorAll('.nav-link')
-// const navbrand=document.querySelector('.navbar-brand')
-// const bars=document.querySelector('.fa-bars')
-// const addBg = () => {
-//     if(window.scrollY>=0){
-//         navbar.classList.add('addBg')
-//         navlink.forEach(nav=>{nav.style.color="black"
-//         nav.addEventListener('mouseenter',()=>{nav.style.color='#ffda79'})
-//         nav.addEventListener('mouseleave',()=>{nav.style.color='black'})
-//     })
-//     }else if(window.scrollY<100){
-//         navbar.classList.remove('addBg')
-//         navbar.style.transition='.3s'
-//         navlink.forEach(nav=>{nav.style.color="black"
-//         nav.addEventListener('mouseenter',()=>{nav.style.color="#ffda79"})
-//         nav.addEventListener('mouseleave',()=>{nav.style.color='black'})
-//     })
-//     }
-// }
-// // document.addEventListener('DOMContentLoaded',addBg)
-// window.addEventListener('DOMContentLoaded',addBg)
+let element=document.querySelector('.contactBtn')
+const movebtn = () => {
+    element.classList.toggle('moveBtn')
+}
+element.addEventListener('mouseenter',movebtn)
+element.addEventListener('mouseleave',movebtn)
