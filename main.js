cc6b303c6d9ec9817d768c06fb481074598e3f9a
Hekari <sszpytma6@gmail.com>
2022-02-11 15:29:37 +0100

    footer to end, contact inputs style needs to be added ASAP, change contact bg to some cool patterns

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 let element=document.querySelector('.contactBtn')
+let element2=document.querySelector('.pricing-btn')
+const addtxt = () => {
+    element.textContent='Skontaktuj się!'
+}
 const movebtn = () => {
     element.classList.toggle('moveBtn')
 }
+const addtxt2 = () => {
+    element2.textContent='Katalog'
+    element2.classList.toggle('moveBtn')
+    
+}
+addtxt()
+addtxt2()
 element.addEventListener('mouseenter',movebtn)
 element.addEventListener('mouseleave',movebtn)
+element2.addEventListener('mouseenter',addtxt2)
+element2.addEventListener('mouseleave',addtxt2)
