1d83f3e39d9f744bb7cb08fe56ea74aa48ce4870
Hekari <sszpytma6@gmail.com>
2022-02-14 22:59:44 +0100

    add style to regulamin/policy/ add script when login obtained

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-let element=document.querySelector('.contactBtn')
-let element2=document.querySelector('.pricing-btn')
+const element=document.querySelector('.contactBtn')
+const element2=document.querySelector('.pricing-btn')
+const logo=document.querySelector('.navbar-brand')
+const about=document.querySelectorAll('.card')
 const addtxt = () => {
     element.textContent='Skontaktuj się!'
 }
@@ -11,9 +13,16 @@ const addtxt2 = () => {
     element2.classList.toggle('moveBtn')
     
 }
+const Animations = () => {
+    if(window.scrollY<=0){
+        logo.classList.toggle('animate__animated')
+        logo.classList.toggle('animate__fadeIn')
+    }
+}
 addtxt()
 addtxt2()
 element.addEventListener('mouseenter',movebtn)
 element.addEventListener('mouseleave',movebtn)
 element2.addEventListener('mouseenter',addtxt2)
 element2.addEventListener('mouseleave',addtxt2)
+logo.addEventListener('mouseenter',Animations)
